389c01ea3c7a4545e69f4421f168e07c4df78335
Jess.Vel <jess.vel92@gmail.com>
2020-07-04 17:01:26 -0300

    inicializando repositorio

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from './componentes/Header';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+        <Header />
+        <Header />
+        
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         dgfsgfgfdg
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
